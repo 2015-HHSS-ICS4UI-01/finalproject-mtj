9d8b31ec86c37ce3ce3f25db0fa26374b66124d8
Jacob <Jacob@Jacob-School>
2016-01-18 11:31:35 -0500

    Gui Stuff

--- core/src/com/rts/screens/AssetManager.java
+++ core/src/com/rts/screens/AssetManager.java
@@ -35,6 +35,9 @@ public class AssetManager {
     public static Texture baseBlueDamage = new Texture("BaseBlueDamage.png");
     public static Texture baseRed = new Texture("BaseRed.png");
     public static Texture baseRedDamage = new Texture("BaseRedDamage.png");
+    public static Texture GUI = new Texture("GUI.png");
+    public static Texture GUICoolDown = new Texture("GUICoolDown.png");
+    
     
     
     
